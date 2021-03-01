bc2beb39cce234b29de5c3f38204746d59cd3af4
HutterSven <64769368+HutterSven@users.noreply.github.com>
2021-03-01 16:01:52 +0100

    put scripts from index and q2 to scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function evaluateQuestion(page) {
+    if (location.search.substring(1).localeCompare("")){
+        var points = 0;
+    }else{
+        var points = eval(location.search.substring(1));
+    }
+    var radios = document.getElementsByName('antwort');
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            if(radios[i].value == 1){
+                points++;
+            }
+        }
+    }
+    var nextPage = './q'+page+'.html?p='+points
+    window.open(nextPage,'_self')
+}
