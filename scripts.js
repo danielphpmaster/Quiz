113641fbb56c471b0ba813f3ed28e50a3be487e0
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2021-03-17 21:42:54 +0100

    Questions are now saved in a json file.

--- scripts.js
+++ scripts.js
@@ -1,131 +1,19 @@
-const questions =
-    [
-        {
-            "question": "What is the scientific name of a butterfly?",
-            "answers": [
-                "Apis",
-                "Coleoptera",
-                "Formicidae",
-                "Rhopalocera"
-            ],
-            "correctIndex": 3
-        },
-        {
-            "question": "How hot is the surface of the sun?",
-            "answers": [
-                "1,233 K",
-                "5,778 K",
-                "12,130 K",
-                "101,300 K"
-            ],
-            "correctIndex": 1
-        },
-        {
-            "question": "Who are the actors in The Internship?",
-            "answers": [
-                "Ben Stiller, Jonah Hill",
-                "Courteney Cox, Matt LeBlanc",
-                "Kaley Cuoco, Jim Parsons",
-                "Vince Vaughn, Owen Wilson"
-            ],
-            "correctIndex": 3
-        },
-        {
-            "question": "What is the capital of Spain?",
-            "answers": [
-                "Berlin",
-                "Buenos Aires",
-                "Madrid",
-                "San Juan"
-            ],
-            "correctIndex": 2
-        },
-        {
-            "question": "What are the school colors of the University of Texas at Austin?",
-            "answers": [
-                "Black, Red",
-                "Blue, Orange",
-                "White, Burnt Orange",
-                "White, Old gold, Gold"
-            ],
-            "correctIndex": 2
-        },
-        {
-            "question": "What is 70 degrees Fahrenheit in Celsius?",
-            "answers": [
-                "18.8889",
-                "20",
-                "21.1111",
-                "158"
-            ],
-            "correctIndex": 2
-        },
-        {
-            "question": "When was Mahatma Gandhi born?",
-            "answers": [
-                "October 2, 1869",
-                "December 15, 1872",
-                "July 18, 1918",
-                "January 15, 1929"
-            ],
-            "correctIndex": 0
-        },
-        {
-            "question": "How far is the moon from Earth?",
-            "answers": [
-                "7,918 miles (12,742 km)",
-                "86,881 miles (139,822 km)",
-                "238,400 miles (384,400 km)",
-                "35,980,000 miles (57,910,000 km)"
-            ],
-            "correctIndex": 2
-        },
-        {
-            "question": "What is 65 times 52?",
-            "answers": [
-                "117",
-                "3120",
-                "3380",
-                "3520"
-            ],
-            "correctIndex": 2
-        },
-        {
-            "question": "How tall is Mount Everest?",
-            "answers": [
-                "6,683 ft (2,037 m)",
-                "7,918 ft (2,413 m)",
-                "19,341 ft (5,895 m)",
-                "29,029 ft (8,847 m)"
-            ],
-            "correctIndex": 3
-        },
-        {
-            "question": "When did The Avengers come out?",
-            "answers": [
-                "May 2, 2008",
-                "May 4, 2012",
-                "May 3, 2013",
-                "April 4, 2014"
-            ],
-            "correctIndex": 1
-        },
-        {
-            "question": "What is 48,879 in hexidecimal?",
-            "answers": [
-                "0x18C1",
-                "0xBEEF",
-                "0xDEAD",
-                "0x12D591"
-            ],
-            "correctIndex": 1
-        }
-    ]
-const length = questions.length;
 let questNumber = -1;
 let points = 0;
 let selectedAnswer;
 var timeOut;
+var quests;
+let length;
+
+function fetchQuestions() {
+    return fetch('questions.json')
+        .then(res => res.json())
+        .then(function(data) {
+            quests = data;
+            length = quests.length;
+            fillQuestions();
+        })
+}
 
 function enableButton() {
     document.getElementById("button").disabled = false;
@@ -143,7 +31,7 @@ function calculatePoints() {
             break;
         }
     }
-    if (selectedAnswer == questions[questNumber].correctIndex) {
+    if (selectedAnswer == quests[questNumber].correctIndex) {
         points++;
         document.getElementById("score").innerHTML = points;
     }
@@ -157,12 +45,12 @@ function fillQuestions() {
 
     questNumber++; // Math.floor(Math.random() * length);
 
-    if (questNumber < questions.length) {
-        document.getElementById("question").innerHTML = questions[questNumber].question;
-        document.getElementById("a1").innerHTML = questions[questNumber].answers[0];
-        document.getElementById("a2").innerHTML = questions[questNumber].answers[1];
-        document.getElementById("a3").innerHTML = questions[questNumber].answers[2];
-        document.getElementById("a4").innerHTML = questions[questNumber].answers[3];
+    if (questNumber < length) {
+        document.getElementById("question").innerHTML = quests[questNumber].question;
+        document.getElementById("a1").innerHTML = quests[questNumber].answers[0];
+        document.getElementById("a2").innerHTML = quests[questNumber].answers[1];
+        document.getElementById("a3").innerHTML = quests[questNumber].answers[2];
+        document.getElementById("a4").innerHTML = quests[questNumber].answers[3];
 
         timer();
     } else {
@@ -179,6 +67,7 @@ function timeBar() {
         bar.classList.add("round-time-bar");
     });
 }
+
 function timer() {
     timeOut = setTimeout(function () {
         endScreen("Time is over!")
